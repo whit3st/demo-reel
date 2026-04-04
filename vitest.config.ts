fb69826ce6b11f7a3e1923ef88eb7a9e06d6d5a1
Alican Akyildiz <alican15033@gmail.com>
2026-04-04 18:16:58 +0300

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    include: ['test/**/*.test.ts'],
-    testTimeout: 30000,
-    hookTimeout: 30000,
+    environment: "node",
+    include: ["test/**/*.test.ts"],
+    testTimeout: 10000,
+    hookTimeout: 10000,
   },
 });
